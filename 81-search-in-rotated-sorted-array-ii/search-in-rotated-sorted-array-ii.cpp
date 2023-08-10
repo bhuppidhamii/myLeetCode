e689c3cc0fb81af65c30b77ada24ec69ebb6c515
Bhupendra Dhami <dhamibhupendra3@gmail.com>
2023-08-10 07:58:16 +0530

    Time: 7 ms (53.92%) | Memory: 14.1 MB (18.44%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        for(auto i:nums){
+            if(i==target){
+                return true;
+            }
+        }
+        return false;
+    }
+};
