797ab299a5438e7df4f1ce3056f4aa2dad326238
Bhupendra Dhami <dhamibhupendra3@gmail.com>
2023-08-11 20:53:59 +0530

    Time: 3 ms (50.93%) | Memory: 5.9 MB (88.31%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countOperations(int num1, int num2) {
         int count=0;
-        while(num1 != 0 && num2 !=0 && num1 >= 0 && num2 >=0){
+        while(num1 != 0 && num2 !=0){
             if(num1 >= num2){
                 num1-=num2;
             }else{
