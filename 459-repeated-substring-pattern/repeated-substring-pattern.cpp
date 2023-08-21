9fbe90a988864b8f801b42a0e13d7396ee26e48c
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-08-21 16:31:55 +0530

    Time: 19 ms (64.17%) | Memory: 44.6 MB (28.95%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        string ss=s+s;
-        string subString=ss.substr(1, ss.length()-2);
-
-        if(subString.find(s) != string::npos){
-            return true;
-        }else{
-            return false;
+        int l=s.size();
+        for(int i=l/2;i>=1;i--){
+            if(l % i == 0){
+                int num_repeats=l/i;
+                string substring=s.substr(0,i);
+                string temp="";
+                for(int j=0;j<num_repeats;j++){
+                    temp+=substring;
+                }
+                if(temp == s){
+                    return true;
+                }
+            }
         }
+        return false;
     }
-
 };
