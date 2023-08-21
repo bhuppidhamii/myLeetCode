e343808b1a11363bc196c27d2ed1dd386daa4bf6
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-08-21 16:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (24.71%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int idx=-1;
-        if(haystack.find(needle)!=string::npos){
-            return haystack.find(needle);
-        }else{
-            return idx;
-        }
+        return haystack.find(needle);
+
     }
 };
