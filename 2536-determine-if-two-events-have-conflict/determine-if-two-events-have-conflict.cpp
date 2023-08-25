288ba8b6f41ef8da946e46f8ee4d9bad1aa44be2
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-08-25 17:19:52 +0530

    Time: 5 ms (19.50%) | Memory: 11.6 MB (17.76%) - LeetSync

---
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    int timeConverter(string s){
+        // HH:MM
+        int hours = stoi(s.substr(0, 2));
+        int minutes = stoi(s.substr(3, 2));
+
+        return hours * 60 + minutes; // Convert time to minutes
+    }
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        int ev1ST = timeConverter(event1[0]);
+        int ev1ET = timeConverter(event1[1]);
+
+        int ev2ST = timeConverter(event2[0]);
+        int ev2ET = timeConverter(event2[1]);
+
+        // Check if the time ranges overlap in any way
+        if ((ev1ST <= ev2ST && ev2ST <= ev1ET) || 
+            // event2 starts within event1
+            (ev1ST <= ev2ET && ev2ET <= ev1ET) || 
+            // event2 ends within event1
+            (ev2ST <= ev1ST && ev1ST <= ev2ET) || 
+            // event1 starts within event2
+            (ev2ST <= ev1ET && ev1ET <= ev2ET))   
+            // event1 ends within event2
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
