719bea2d49359ee5fa4136a19eceb934fcb73f1d
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-08-26 08:54:32 +0530

    Time: 32 ms (72.44%) | Memory: 18.9 MB (71.70%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,23 +5,18 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
 
-        for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
-            }else{
-                for(int j=i+1;j<n;j++){
-                    if(intervals[j][0] <= end){
-                        end=max(end, intervals[j][1]);
-                    }else{
-                        break;
-                    }
-                }
-            }
-            ans.push_back({start, end});
-        }
+       for(int i=0;i<n;i++){
+           if(ans.empty()){
+               ans.push_back( {intervals[i][0], intervals[i][1]} );
+           }else{
+               if(intervals[i][0] <= ans.back()[1]){
+                   ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+               }else{
+                   ans.push_back( {intervals[i][0], intervals[i][1]} );
+               }
+           }
+       }
+        
         return ans;
     }
 };
