caf0a98fcf2f0b740b6c3e4a273c0f375e46b570
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-09-05 23:15:03 +0530

    Time: 2 ms (54.62%) | Memory: 5.9 MB (52.83%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return floor(sqrt(n));        
+        //count the total no of perfect squares till n       
+        return sqrt(n);
     }
 };
