2be27de533d32a33015d28bdf88a77c3bd92e553
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-09-07 08:32:54 +0530

    Time: 4 ms (38.20%) | Memory: 7.6 MB (12.22%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -1,30 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
+private:
+    ListNode* reverseLL(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* forw = head;
+
+        while (curr != nullptr) {
+            forw = forw->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        vector<int>arr;
-        ListNode*temp=head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp=temp->next;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+
+        for (int i = 1; i < left; i++) {
+            prev = prev->next;
         }
-        reverse(arr.begin()+(left-1), arr.begin()+(right));
-        ListNode* dummy=new ListNode(0);
-        temp=dummy;
-        for(auto i:arr){
-            ListNode* newNode=new ListNode(i);
-            temp->next=newNode;
-            temp=temp->next;
+
+        ListNode* start = prev->next;
+        ListNode* end = start;
+
+        for (int i = left; i < right; i++) {
+            end = end->next;
         }
+
+        ListNode* lastPart = end->next;
+        end->next = nullptr;
+
+        // Update 'prev' before reversing the sublist
+        prev->next = nullptr;
+
+        ListNode* reversed = reverseLL(start);
+        prev->next = reversed;
+        start->next = lastPart;
+
         return dummy->next;
     }
 };
