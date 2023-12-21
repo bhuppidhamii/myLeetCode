6942f832e46cf9ff0c3e5282615afda13ddc4822
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-12-21 06:59:41 +0530

    Time: 2497 ms (5.03%) | Memory: 42.7 MB (44.91%) - LeetSync

---
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        ListNode* temp=head;
+        vector<int>v;
+        while(temp!=nullptr){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+
+        int N=v.size();
+        vector<int>ans(N,0);
+        for(int i=0; i<N; i++){
+            for(int j=i+1; j<N; j++){
+                if(v[j] > v[i]){
+                    ans[i] = v[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
