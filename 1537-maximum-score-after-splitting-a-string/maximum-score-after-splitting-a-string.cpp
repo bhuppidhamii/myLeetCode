c55b6c26d8a647906dd5250fe3e586e0d2582ec5
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-12-22 06:48:38 +0530

    Time: 4 ms (23.21%) | Memory: 7.2 MB (11.16%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,22 +1,30 @@
 class Solution {
-private:
-    int count1(string str) {
-        int cnt = 0;
-        for (auto ch : str) {
-            if (ch == '1')
-                cnt++;
-        }
-        return cnt;
-    }
 public:
     int maxScore(string s) {
         int N = s.size();
+
+        vector<int> prefix0(N, 0);
+        int cnt0 = 0;
+        for (int i = 0; i < N; i++) {
+            if (s[i] == '0') {
+                cnt0++;
+            }
+            prefix0[i] = cnt0;
+        }
+
+        vector<int> prefix1(N, 0);
+        int cnt1 = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (s[i] == '1') {
+                cnt1++;
+            }
+            prefix1[i] = cnt1;
+        }
+
         int ans = INT_MIN;
         for (int i = 0; i < N - 1; i++) {
-            string left = s.substr(0, i + 1);
-            string right = s.substr(i + 1, N - (i + 1));
-            int sum = (left.size() - count1(left)) + count1(right);
-            ans = max(ans, sum);
+            int sum = prefix0[i] + prefix1[i + 1];
+            ans = max(sum, ans);
         }
         return ans;
     }
