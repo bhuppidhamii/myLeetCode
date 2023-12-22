15e7182d73f643d2dfb6f0b57877ec713d3fe7c8
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-12-22 06:36:05 +0530

    Time: 18 ms (5.80%) | Memory: 14.9 MB (6.25%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,16 +1,10 @@
 class Solution {
 private:
-    int count1(string str){
-        int cnt=0;
-        for(auto ch:str){
-            if(ch=='1') cnt++;
-        }
-        return cnt;
-    }
-    int count0(string str){
-        int cnt=0;
-        for(auto ch:str){
-            if(ch=='0') cnt++;
+    int count1(string str) {
+        int cnt = 0;
+        for (auto ch : str) {
+            if (ch == '1')
+                cnt++;
         }
         return cnt;
     }
@@ -18,12 +12,10 @@ public:
     int maxScore(string s) {
         int N = s.size();
         int ans = INT_MIN;
-        for(int i=0; i<N-1; i++){
-            string left=s.substr(0, i+1);
-            cout<<left<<" ";
-            string right=s.substr(i+1, N-(i+1));
-            cout<<right<<endl;
-            int sum= count0(left) + count1(right);
+        for (int i = 0; i < N - 1; i++) {
+            string left = s.substr(0, i + 1);
+            string right = s.substr(i + 1, N - (i + 1));
+            int sum = (left.size() - count1(left)) + count1(right);
             ans = max(ans, sum);
         }
         return ans;
