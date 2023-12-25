3b6c6cca2ee39602a34be22cd9085ae52b845ce9
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-12-25 06:49:46 +0530

    Time: 8 ms (54.83%) | Memory: 11 MB (83.23%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        map<int, int> m;
-        for (auto& i : nums1) {
-            int key = i[0];
-            int val = i[1];
-            if (m.find(key) == m.end()) {
-                m.insert({key, val});
-            }
-        }
-        for (auto& i : nums2) {
-            int key = i[0];
-            int val = i[1];
-            if (m.find(key) != m.end()) {
-                // already present
-                int v = m[key];
-                m.erase(key);
-                v += val;
-                m.insert({key, v});
-            } else {
-                m.insert({key, val});
-            }
-        }
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
+        vector<vector<int>>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0, j = 0, N1 = nums1.size(), N2 = nums2.size();
         vector<vector<int>> ans;
-        for (auto i : m) {
-            ans.push_back({i.first, i.second});
+        while (i < N1 && j < N2) {
+            int keyi = nums1[i][0], keyj = nums2[j][0];
+            int vali = nums1[i][1], valj = nums2[j][1];
+            if (keyi == keyj) {
+                ans.push_back({keyi, vali + valj});
+                i++, j++;
+            } else if (keyi < keyj) {
+                ans.push_back({keyi, vali});
+                i++;
+            } else {
+                ans.push_back({keyj, valj});
+                j++;
+            }
+        }
+        while (i < N1) {
+            ans.push_back({nums1[i][0], nums1[i][1]});
+            i++;
+        }
+        while (j < N2) {
+            ans.push_back({nums2[j][0], nums2[j][1]});
+            j++;
         }
         return ans;
     }
