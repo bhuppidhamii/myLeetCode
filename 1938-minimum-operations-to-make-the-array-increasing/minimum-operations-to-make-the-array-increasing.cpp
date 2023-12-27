782efb9e0d75850f0024522f0b54e2db22a018be
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-12-27 06:18:39 +0530

    Time: 8 ms (86.59%) | Memory: 16.3 MB (11.98%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -3,9 +3,10 @@ public:
     int minOperations(vector<int>& nums) {
         int cnt = 0;
         for (int i = 0; i < nums.size() - 1; i++) {
-            while(nums[i+1]<=nums[i]){
-                nums[i+1]+=1;
-                cnt++;
+            if (nums[i + 1] <= nums[i]) {
+                int diff = nums[i] - nums[i + 1];
+                cnt += (diff + 1);
+                nums[i + 1] += (diff + 1);
             }
         }
         return cnt;
