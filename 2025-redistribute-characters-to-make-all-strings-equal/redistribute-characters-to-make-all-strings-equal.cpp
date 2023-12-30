29fff174cae21ad0592387465e43189628207ff0
Bhupender Singh <dhamibhupendra3@gmail.com>
2023-12-30 05:57:59 +0530

    Time: 36 ms (5.06%) | Memory: 12.7 MB (10.12%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        map<char, int> m;
+        for (auto w : words) {
+            for (auto ch : w) {
+                m[ch]++;
+            }
+        }
+        int N = words.size();
+        for (auto i : m) {
+            if (i.second % N != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
