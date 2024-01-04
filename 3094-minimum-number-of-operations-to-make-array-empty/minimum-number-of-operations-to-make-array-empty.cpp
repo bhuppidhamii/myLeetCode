507e17336009a04294deb99542b3faf7171cbe66
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-01-04 11:17:06 +0530

    Time: 142 ms (17.50%) | Memory: 85.9 MB (9.02%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        map<int, int> m;
-        for (auto i : nums) {
+        map<int,int>m;
+        for(auto i:nums){
             m[i]++;
         }
-
-        int count = 0;
-        while (m.size() > 0) {
-            for (auto i : m) {
-                int num = i.first;
-                int freq = i.second;
-                if (freq == 1){
-                    return -1;
-                }
-                if (freq % 3 == 0) {
-                    freq -= 3;
-                } else {
-                    freq -= 2;
-                }
-                if (freq == 0) {
-                    // remove from map
-                    m.erase(num);
-                } else {
-                    // update
-                    m[num] = freq;
-                }
-                count++;
+        int ans=0;
+        for(auto i:m){
+            if(i.second==1){
+                return -1;
             }
+            int freq=i.second;
+            ans+=ceil((double)freq/3);
         }
-        return count;
+        return ans;
     }
 };
