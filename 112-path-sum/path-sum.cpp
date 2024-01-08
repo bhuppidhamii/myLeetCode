de0248d6e29a70507ba9da255717c76c28aa1716
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-01-08 08:49:58 +0530

    Time: 4 ms (86.33%) | Memory: 21.6 MB (39.27%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    bool inOrder(TreeNode* root, int sum, int targetSum) {
-        if (root == nullptr)
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) {
             return false;
-
-        sum += root->val;
-
-        if (root->left == nullptr && root->right == nullptr) {
-            return sum == targetSum;
         }
 
-        return inOrder(root->left, sum, targetSum) || inOrder(root->right, sum, targetSum);
-    }
+        targetSum -= root->val;
 
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum = 0;
-        return inOrder(root, sum, targetSum);
+        // Check if it's a leaf node and if the remaining targetSum is 0
+        if (root->left == nullptr && root->right == nullptr) {
+            return targetSum == 0;
+        }
+
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
     }
 };
