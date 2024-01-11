1a775feed8b877da4cf5e6c5aad85bd4fc7aa618
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-01-11 06:30:34 +0530

    Time: 7 ms (42.40%) | Memory: 11.3 MB (26.70%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void solve(TreeNode* root, vector<int> v) {
-        if (root == NULL)
-            return;
-
-        v.push_back(root->val);
-        if (root->left == NULL && root->right == NULL) {
-            ans.push_back(v);
+    vector<int> ans;
+    void solve(TreeNode* root, int mini, int maxi) {
+        if (root == NULL) {
             return;
         }
 
-        solve(root->left, v);
-        solve(root->right, v);
+        mini = min(root->val, mini); 
+        maxi = max(root->val, maxi); 
+        
+        if (root->left == NULL && root->right == NULL) {
+            ans.push_back(abs(maxi - mini));
+            return;
+        }
+
+        solve(root->left, mini, maxi);
+        solve(root->right, mini, maxi);
     }
     int maxAncestorDiff(TreeNode* root) {
-        vector<int> v;
-        solve(root, v);
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        solve(root, mini, maxi);
 
-        for (int i = 0; i < ans.size(); i++) {
-            for (int j = 0; j < ans[i].size(); j++) {
-                cout << ans[i][j] << " ";
-            }
-            cout << endl;
-        }
-        // return 0;
-
-        vector<int> m;
-        for (int i = 0; i < ans.size(); i++) {
-
-            int maxi = INT_MIN;
-            for (int j = 0; j < ans[i].size(); j++) {
-                for (int k = j + 1; k < ans[i].size(); k++) {
-                    int diff = abs(ans[i][j] - ans[i][k]);
-                    maxi = max(maxi, diff);
-                }
-            }
-            m.push_back(maxi);
-        }
-        return *max_element(m.begin(), m.end());
+        return *max_element(ans.begin(), ans.end());
     }
 };
