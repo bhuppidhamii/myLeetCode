0d40788774fb038503210cf0ce09b118ebea9f35
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-01-14 20:20:38 +0530

    Time: 70 ms (84.64%) | Memory: 20.8 MB (96.46%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -4,36 +4,27 @@ public:
         int l1 = word1.length(), l2 = word2.length();
         if (l1 != l2) return false;
 
-        // jo char word1 m h vo char word2 m bhi hona chahiye!
-        // frequencies bhi same honi chahiye!
-
-        map<char, int> m1;
-        for (auto ch : word1) {
-            m1[ch]++;
+        vector<int>freq1(26,0);
+        for(auto ch:word1){
+            int idx=ch-'a';
+            freq1[idx]++;
         }
 
-        map<char, int> m2;
-        for (auto ch : word2) {
-            m2[ch]++;
+        vector<int>freq2(26,0);
+        for(auto ch:word2){
+            int idx=ch-'a';
+            freq2[idx]++;
         }
 
-        vector<int> freq1;
-        for (auto i : m1) {
-            char ch = i.first;
-            freq1.push_back(i.second);
-            if (m2.find(ch) == m2.end()) {
+        for(int i=0;i<26;i++){
+            if(!((freq1[i]!=0 && freq2[i]!=0) || (freq1[i]==0 && freq2[i]==0))){
                 return false;
             }
         }
+        
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
 
-        vector<int> freq2;
-        for (auto i : m2) {
-            freq2.push_back(i.second);
-        }
-
-        sort(begin(freq1), end(freq1));
-        sort(begin(freq2), end(freq2));
-
-        return freq1 == freq2;
+        return freq1==freq2;
     }
 };
