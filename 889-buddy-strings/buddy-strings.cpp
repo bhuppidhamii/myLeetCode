a8f042014761ee2096e7ca729cd5d2f0c23423f7
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-01-17 09:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (46.48%) - LeetSync

--- 889-buddy-strings/buddy-strings.cpp
+++ 889-buddy-strings/buddy-strings.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    bool checkFreq(string &s){ 
-        // check if any char has freq 2
-        map<char,int>m;
-        for(auto ch:s){
-            m[ch]++;
-        }
-        for(auto i:m){
-            if(i.second>=2){
+    bool checkFreq(string& s) {
+        vector<int> freq(26);
+        for (auto ch : s) {
+            freq[ch - 'a']++;
+
+            if (freq[ch - 'a'] > 1) {
                 return true;
             }
         }
         return false;
     }
     bool buddyStrings(string s, string goal) {
-        if(s.length()!=goal.length()) return false;
+        if (s.length() != goal.length())
+            return false;
 
-        if(s==goal){
-            if(checkFreq(s)){
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            // kon konse indices hai jaha s ka char!=gaol ka char 
-            // sirf 2 indexes not equal hone chahiye
-            vector<int>idx;
-            for(int i=0;i<s.length();i++){
-                if(s[i]!=goal[i]){
+        if (s == goal) {
+            return checkFreq(s);
+        } else {
+            vector<int> idx;
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] != goal[i]) {
                     idx.push_back(i);
                 }
             }
-            if(idx.size()==2){
-                swap(s[idx[0]],s[idx[1]]);
-                if(s==goal){
+            if (idx.size() == 2) {
+                swap(s[idx[0]], s[idx[1]]);
+                if (s == goal) {
                     return true;
                 }
             }
