330e21120fa64679a8a4eaee0c93d433aa21ca00
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-02-01 11:59:25 +0530

    Time: 121 ms (99.70%) | Memory: 119.4 MB (39.48%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -4,18 +4,13 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
 
-        vector<vector<int>> ans(n / 3, vector<int>(3, 0));
-        int idx = 0;
+        vector<vector<int>> ans;
         for (int i = 0; i < n; i += 3) {
             if (nums[i + 2] - nums[i] > k) {
                 return {};
             } else {
-                int j = i;
-                for (int k = 0; k < 3; k++) {
-                    ans[idx][k] = nums[j++];
-                }
+                ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
             }
-            idx++;
         }
         return ans;
     }
