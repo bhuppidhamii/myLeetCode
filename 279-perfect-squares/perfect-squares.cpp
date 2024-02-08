239e0ba0749d92c1c7bbda58b38d93b818f75f6f
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-02-08 08:57:48 +0530

    Time: 58 ms (82.56%) | Memory: 8.1 MB (87.94%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int t[10001];
+    int solve(int n) {
+        if (n <= 0)
+            return 0;
+        if (t[n] != -1) {
+            return t[n];
+        }
+
+        int minCount = INT_MAX;
+
+        for (int i = 1; i * i <= n; i++) {
+            int result = 1 + solve(n - i * i);
+            minCount = min(minCount, result);
+        }
+
+        return t[n] = minCount;
+    }
+
+    int numSquares(int n) {
+        memset(t, -1, sizeof(t));
+        return solve(n);
+    }
+};
