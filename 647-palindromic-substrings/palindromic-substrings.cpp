0ff8581ab9203a66a1b7692eb8971bae06e6be0c
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-02-10 10:12:42 +0530

    Time: 30 ms (23.94%) | Memory: 12 MB (52.79%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    bool solve(int i, int j, string &s) {
+    int t[1001][1001];
+    bool solve(int i, int j, string& s) {
         if (i >= j)
             return true;
-        if (s[i] == s[j]) {
-            return solve(i + 1, j - 1, s);
+
+        if (t[i][j] != -1) {
+            return t[i][j];
         }
-        return false;
+        if (s[i] == s[j]) {
+            return t[i][j] = solve(i + 1, j - 1, s);
+        }
+        return t[i][j] = false;
     }
     int countSubstrings(string s) {
         int n = s.size();
         int i = 0, j = n - 1;
+        memset(t, -1, sizeof(t));
 
-        int cnt=0;
+        int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 if (solve(i, j, s) == true) { // if substr is palindrome
