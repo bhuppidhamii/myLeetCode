8a03a2a69e1ec13597a8ec217534b1e7f42dbe85
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-02-12 08:39:25 +0530

    Time: 7 ms (100.00%) | Memory: 41.9 MB (20.00%) - LeetSync

--- 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
-        int n = nums.size();
-        int m = pattern.size();
-        int ans = 0;
+        int n = nums.size(), m = pattern.size(), ans = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (j - i + 1 == m + 1) {
-                    int flag = 1;
-                    int l = i;
-                    for (int k = 0; k < m; k++, l++) {
-                        if (pattern[k] == 1 && nums[l + 1] > nums[l]) {
-                            continue;
-                        } else if (pattern[k] == 0 && nums[l + 1] == nums[l]) {
-                            continue;
-                        } else if (pattern[k] == -1 && nums[l + 1] < nums[l]) {
-                            continue;
-                        } else {
-                            flag = 0;
-                            break;
-                        }
-                    }
-                    if (flag == 1) {
-                        ans++;
-                    }
+            if (i + m >= n) break;
+            int flag = 1;
+            for (int j = i, k = 0; j + 1 < n, k < m; j++, k++) {
+                if (pattern[k] == 1 && nums[j + 1] > nums[j]) {
+                    continue;
+                } else if (pattern[k] == 0 && nums[j + 1] == nums[j]) {
+                    continue;
+                } else if (pattern[k] == -1 && nums[j + 1] < nums[j]) {
+                    continue;
+                } else {
+                    flag = 0;
+                    break;
                 }
+            }
+            if (flag == 1) {
+                ans++;
             }
         }
         return ans;
