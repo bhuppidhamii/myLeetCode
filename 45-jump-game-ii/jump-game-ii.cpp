fbe6d4cead39959e8b51830dcbbdbbef78a8d7db
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-02-25 08:44:53 +0530

    Time: 298 ms (13.96%) | Memory: 25.5 MB (5.50%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int n;
-    int t[10001];
-    long long int solve(int idx, vector<int>& nums) {
-        if (idx == n - 1)
+    long long int solve(int idx, vector<int>& nums, vector<int>& t) {
+        if (idx == nums.size() - 1)
             return 0;
-        if (idx >= n)
-            return 99;
         if (t[idx] != -1) {
             return t[idx];
         }
         long long int minJumps = INT_MAX;
-        for (int i = 1; i <= nums[idx]; i++) {
-            minJumps = min(minJumps, 1 + solve(i + idx, nums));
+        for (int i = 1; i <= nums[idx] && i + idx < nums.size(); i++) {
+            minJumps = min(minJumps, 1 + solve(i + idx, nums, t));
             t[idx] = minJumps;
         }
         return t[idx] = minJumps;
     }
     int jump(vector<int>& nums) {
-        memset(t, -1, sizeof(t));
-        n = nums.size();
-        return solve(0, nums);
+        vector<int> t(10001, -1);
+        return solve(0, nums, t);
     }
 };
