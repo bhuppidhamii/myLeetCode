de91238393453a6e7751f7086128c945a1f753dd
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-03-11 08:23:45 +0530

    Time: 4 ms (26.39%) | Memory: 8 MB (44.45%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     string customSortString(string order, string s) {
         unordered_map<char, int> m;
-        for (char i : s) {
+        for (char i : s)
             m[i]++;
-        }
 
         string ans = "";
         for (char ch : order) {
             if (m.find(ch) != m.end()) {
-                for (int i = 0; i < m[ch]; i++) {
-                    ans += ch;
-                }
+                ans += string(m[ch], ch);
                 m.erase(ch);
             }
         }
-        
-        for (auto i : m) {
-            for (int j = 0; j < i.second; j++) {
-                ans += i.first;
-            }
-        }
+
+        for (auto i : m)
+            ans += string(i.second, i.first);
+
         return ans;
     }
 };
