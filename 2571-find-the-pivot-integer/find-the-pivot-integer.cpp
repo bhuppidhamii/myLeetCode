bfd3506678a087fa468b8d13193b11aa2f8c24ab
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-03-13 08:11:27 +0530

    Time: 3 ms (51.39%) | Memory: 7.2 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int total = (n * (n + 1)) / 2;
-        int currSum = 0;
-        for (int i = 1; i <= n; i++) {
-            currSum += i;
-            if (currSum == ((total - currSum) + i)) {
-                return i;
-            }
+        int sum = (n * (n + 1)) / 2;
+        int root = sqrt(sum);
+        if (root * root == sum) {
+            return root;
         }
         return -1;
     }
