22cda9d8a1f96bedb37036384a6e74e8aa76e5d8
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-05-01 08:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (26.16%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-       int N=word.length();
-       int idx=word.find(ch);
-       string one=word.substr(0,idx+1);
-       reverse(one.begin(),one.end());
-       string two=word.substr(idx+1);
-       return one+two;
+        if (word.find(ch) == string::npos) {
+            return word;
+        }
+        int idx = word.find(ch);
+        string str = word.substr(0, idx + 1);
+        reverse(str.begin(), str.end());
+
+        return str + word.substr(idx + 1);
     }
 };
