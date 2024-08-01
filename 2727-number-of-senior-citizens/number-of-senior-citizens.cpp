462f22cd07165cc977834f179fa854ae43cdf504
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-08-01 10:50:21 +0530

    Time: 3 ms (91.87%) | Memory: 17.7 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -3,8 +3,10 @@ public:
     int countSeniors(vector<string>& details) {
         int cnt = 0;
         for (auto d : details) {
-            int age = (d[11] - '0') * 10 + (d[12]-'0');
-            if (age > 60){cnt++;}
+            int age = (d[11] - '0') * 10 + (d[12] - '0');
+            if (age > 60) {
+                cnt++;
+            }
         }
         return cnt;
     }
