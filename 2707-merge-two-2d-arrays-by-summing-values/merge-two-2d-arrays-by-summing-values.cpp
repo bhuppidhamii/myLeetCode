3ad7affc7ccb17ff22d8de294b2208852dfdaba9
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-03-02 19:18:20 +0530

    Time: 1 ms (45.39%) | Memory: 15.2 MB (28.71%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
-        vector<vector<int>>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+                                    vector<vector<int>>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
 
-        int i = 0, j = 0, N1 = nums1.size(), N2 = nums2.size();
+        map<int, int> mp;
+        for (auto& i : nums1) {
+            mp[i[0]] += i[1];
+        }
+        for (auto& i : nums2) {
+            mp[i[0]] += i[1];
+        }
+
         vector<vector<int>> ans;
-        while (i < N1 && j < N2) {
-            int keyi = nums1[i][0], keyj = nums2[j][0];
-            int vali = nums1[i][1], valj = nums2[j][1];
-            if (keyi == keyj) {
-                ans.push_back({keyi, vali + valj});
-                i++, j++;
-            } else if (keyi < keyj) {
-                ans.push_back({keyi, vali});
-                i++;
-            } else {
-                ans.push_back({keyj, valj});
-                j++;
-            }
-        }
-        while (i < N1) {
-            ans.push_back({nums1[i][0], nums1[i][1]});
-            i++;
-        }
-        while (j < N2) {
-            ans.push_back({nums2[j][0], nums2[j][1]});
-            j++;
+        for (auto& i : mp) {
+            ans.push_back({i.first, i.second});
         }
         return ans;
     }
