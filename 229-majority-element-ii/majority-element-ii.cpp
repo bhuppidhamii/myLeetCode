a85954544c28fcd19e0d9cfbd3abb00e7327b2e3
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-03-27 21:21:35 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (28.75%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
+    vector<int> majorityElement(vector<int>& arr) {
+        int l = arr.size();
+        
+        // Initialize majority elements
+        int maj1 = -1, maj2 = -1;
+        int count1 = 0, count2 = 0;
 
-        int maj1 = NULL;
-        int count1 = 0;
-
-        int maj2 = NULL;
-        int count2 = 0;
-        int freq = floor(n / 3);
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == maj1)
+        // Finding potential candidates
+        for (int num : arr) {
+            if (num == maj1) {
                 count1++;
-            else if (nums[i] == maj2)
+            } else if (num == maj2) {
                 count2++;
-            else if (count1 == 0) {
-                maj1 = nums[i];
+            } else if (count1 == 0) {
+                maj1 = num;
                 count1 = 1;
             } else if (count2 == 0) {
-                maj2 = nums[i];
+                maj2 = num;
                 count2 = 1;
             } else {
                 count1--;
                 count2--;
             }
         }
-        
-        vector<int> result; 
-        // because atmost two elements can occur more than ⌊n/3 ⌋ in an array of length n
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == maj1)
-                count1++;
-            else if (num == maj2)
-                count2++;
+
+        // Validate the candidates
+        count1 = count2 = 0;
+        for (int num : arr) {
+            if (num == maj1) count1++;
+            else if (num == maj2) count2++; // Avoid double counting
         }
-        if (count1 > floor(n / 3))
-            result.push_back(maj1);
-        if (count2 > floor(n / 3))
-            result.push_back(maj2);
-        return result;
+
+        vector<int> res;
+        if (count1 > l / 3) res.push_back(maj1);
+        if (count2 > l / 3) res.push_back(maj2);
+
+        return res;
     }
 };
