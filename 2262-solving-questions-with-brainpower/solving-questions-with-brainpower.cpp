9369753286532ace090b93a7f40bf7b345049b83
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-04-01 22:46:55 +0530

    Time: 9 ms (39.19%) | Memory: 127.7 MB (16.19%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int n;
+    long long solve(int i, vector<vector<int>>& q, vector<long long>& t) {
+        if (i >= n) {
+            return 0;
+        }
+        if (t[i] != -1) {
+            return t[i];
+        }
+
+        // taken
+        long long taken = q[i][0] + solve(i + q[i][1] + 1, q, t);
+
+        // not taken
+        long long not_taken = solve(i + 1, q, t);
+
+        return t[i] = max(taken, not_taken);
+    }
+    long long mostPoints(vector<vector<int>>& q) {
+        n = q.size();
+        vector<long long> t(n + 1, -1);
+        return solve(0, q, t);
+    }
+};
