ed06930b4c51c1071624103dd079f8b2d30ab95a
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-04-14 07:49:11 +0530

    Time: 3 ms (78.04%) | Memory: 11.1 MB (69.69%) - LeetSync

---
+++ 2566-number-of-unequal-triplets-in-array/number-of-unequal-triplets-in-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        int N = nums.size();
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] &&
+                        nums[j] != nums[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
