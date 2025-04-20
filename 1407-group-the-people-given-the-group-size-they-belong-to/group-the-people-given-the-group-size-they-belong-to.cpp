71e39b1e0b162ae95d68b0cc3b8db2bb30ae4d0b
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-04-20 11:38:53 +0530

    Time: 7 ms (26.10%) | Memory: 18.6 MB (6.48%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,35 +2,19 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         map<int, vector<int>> mp;
-        // size, peoples
         int N = groupSizes.size();
+        vector<vector<int>> res;
+
         for (int i = 0; i < N; i++) {
-            mp[groupSizes[i]].push_back(i);
-            // size --> peoples
-        }
+            int size = groupSizes[i];
+            mp[size].push_back(i);
 
-        // for(auto &i:mp){
-        //     cout<<i.first<<" - ";
-        //     for(auto &n:i.second){
-        //         cout<<n<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-
-        vector<vector<int>> ans;
-        for (auto& i : mp) {
-            int size = i.first;
-            vector<int> v;
-            vector<int> peoples = i.second;
-            for (int idx = 0; idx < peoples.size(); idx++) {
-                // cout<<peoples[idx];
-                v.push_back(peoples[idx]);
-                if (v.size() == size) {
-                    ans.push_back(v);
-                    v.erase(v.begin(),v.end());
-                }
+            if (mp[size].size() == size) {
+                res.push_back(mp[size]);
+                mp.erase(size);
             }
         }
-        return ans;
+
+        return res;
     }
 };
