70d74efe9e3619f341a8c8389cf8923f22c2e8ab
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-04-22 20:55:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.16%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-    void dfs(map<int, vector<int>>& adj, int u, vector<bool>& visited) {
-        if (visited[u] == true) {
-            return;
-        }
+    int N;
+    void dfs(vector<vector<int>>& isConnected, int u, vector<bool>& visited) {
         visited[u] = true;
 
-        for (auto& v : adj[u]) {
-            if (visited[v] == false) {
-                dfs(adj, v, visited);
+        for (int v = 0; v < N; v++) {
+            if (visited[v] == false && isConnected[u][v] == 1) {
+                dfs(isConnected, v, visited);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-
-        map<int, vector<int>> adj;
-        for (int i = 0; i < isConnected.size(); i++) {
-            for (int j = 0; j < isConnected[i].size(); j++) {
-                if (isConnected[i][j] == 1) {
-                    adj[i].push_back(j);
-                }
-            }
-        }
-        // cout << adj.size();
-
-        // for (auto& u : adj) {
-        //     cout << u.first << " - ";
-        //     for (auto& v : u.second) {
-        //         cout << v << " ";
-        //     }
-        //     cout << "\n";
-        // }
-
+        N = isConnected.size();
         int V = isConnected.size(), count = 0;
         vector<bool> visited(V, false);
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
-                dfs(adj, i, visited);
+                dfs(isConnected, i, visited);
                 count++;
             }
         }
