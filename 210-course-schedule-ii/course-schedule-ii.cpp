10738ddc7f5e25f6b8fde680fd314efe98b3e999
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-04-28 09:39:12 +0530

    Time: 7 ms (36.39%) | Memory: 18.8 MB (35.02%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,42 +1,53 @@
 class Solution {
 public:
+    bool dfs(int u, unordered_map<int, vector<int>>& adj, vector<bool>& visited,
+             vector<bool>& inRecursion, stack<int>& st) {
+        visited[u] = true;
+        inRecursion[u] = true;
+
+        for (auto& v : adj[u]) {
+            if (!visited[v]) {
+                if (dfs(v, adj, visited, inRecursion, st)) {
+                    return true;
+                }
+            } else if (inRecursion[v]) {
+                return true; // Found a back edge -> cycle
+            }
+        }
+        inRecursion[u] = false;
+        st.push(u);
+        return false;
+    }
+
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        map<int, vector<int>> adj;
+        unordered_map<int, vector<int>> adj;
         int V = numCourses;
-        vector<int> inDegree(V, 0);
         for (auto& p : prerequisites) {
             int a = p[0];
             int b = p[1];
             // b--->a
             adj[b].push_back(a);
-            inDegree[a]++;
         }
 
-        // topological sort - bfs
-        queue<int> q;
+        // dfs
+        vector<bool> visited(V, false);
+        vector<bool> inRecursion(V, false);
+        stack<int> st;
+
+        bool isCyclePresent = false;
         for (int i = 0; i < V; i++) {
-            if (inDegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<int> ans;
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            for (auto& v : adj[u]) {
-                inDegree[v]--;
-                if (inDegree[v] == 0) {
-                    q.push(v);
+            if (!visited[i]) {
+                isCyclePresent = dfs(i, adj, visited, inRecursion, st);
+                if (isCyclePresent == true) {
+                    return {};
                 }
             }
-            ans.push_back(u);
         }
-
-        if (ans.size() == V) {
-            return ans;
+        vector<int> ans;
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
-        return {};
+        return ans;
     }
 };
