1667f6ed76f61e08f6460ebe80aef0fee682c561
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-05-06 08:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (58.11%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        int N=nums.size();
-        vector<int>ans(N,0);
+        int N = nums.size();
+        // a=bq+r
+        // a/b=q
+        // a%b=r
+        for (int i = 0; i < N; i++) {
+            // convert into an combined format
+            int old_num = nums[i];
+            int new_num = nums[nums[i]];
 
-        for(int i=0; i<N; i++){
-            ans[i]=nums[nums[i]];
+            nums[i] = old_num + N * (new_num % N);
         }
-        return ans;
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = nums[i] / N;
+        }
+        return nums;
     }
 };
