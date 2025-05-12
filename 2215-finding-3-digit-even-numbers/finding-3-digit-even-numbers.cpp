4e2821b32cfff7ce65144c6dadcf276f826c8a84
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-05-12 09:33:21 +0530

    Time: 375 ms (18.60%) | Memory: 13.9 MB (49.57%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    set<int> result;  // to avoid duplicates
-    int N;
-
-    void solve(vector<int>& digits, vector<bool>& used, int num, int count) {
-        if (count == 3) {
-            if (num >= 100 && num % 2 == 0) {
-                result.insert(num);
-            }
-            return;
-        }
-
-        for (int i = 0; i < N; ++i) {
-            if (used[i]) continue;
-
-            // Skip leading 0
-            if (count == 0 && digits[i] == 0) continue;
-
-            used[i] = true;
-            solve(digits, used, num * 10 + digits[i], count + 1);
-            used[i] = false;
-        }
-    }
-
     vector<int> findEvenNumbers(vector<int>& digits) {
-        N = digits.size();
-        vector<bool> used(N, false);
-        solve(digits, used, 0, 0);
-
-        return vector<int>(result.begin(), result.end());
+        int N = digits.size();
+        set<int> st;
+        for (int i = 0; i < N; i++) {
+            int a = digits[i];
+            for (int j = 0; j < N; j++) {
+                int b = digits[j];
+                for (int k = 0; k < N; k++) {
+                    int c = digits[k];
+                    if (i != j && j != k && i != k) {
+                        int num = a * 100 + b * 10 + c * 1;
+                        if (num >= 100 && num < 999 && num % 2 == 0) {
+                            st.insert(num);
+                        }
+                    }
+                }
+            }
+        }
+        vector<int> ans(begin(st), end(st));
+        return ans;
     }
 };
