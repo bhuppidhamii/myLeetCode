c6b3dacfb42318abd68ad5cc153511fcfd7d0421
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-05-12 09:43:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (93.09%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        int N = digits.size();
-        set<int> st;
-        for (int i = 0; i < N; i++) {
-            int a = digits[i];
-            for (int j = 0; j < N; j++) {
-                int b = digits[j];
-                for (int k = 0; k < N; k++) {
-                    int c = digits[k];
-                    if (i != j && j != k && i != k) {
-                        int num = a * 100 + b * 10 + c * 1;
-                        if (num >= 100 && num < 999 && num % 2 == 0) {
-                            st.insert(num);
-                        }
-                    }
-                }
-            }
+        vector<int> mp(10, 0);
+        for (auto& i : digits) {
+            mp[i]++;
         }
-        vector<int> ans(begin(st), end(st));
+        vector<int> ans;
+        for (int i = 1; i <= 9; i++) {
+            if (mp[i] == 0)
+                continue;
+            mp[i]--;
+            for (int j = 0; j <= 9; j++) {
+                if (mp[j] == 0)
+                    continue;
+                mp[j]--;
+                for (int k = 0; k <= 8; k += 2) {
+                    if (mp[k] == 0)
+                        continue;
+                    mp[k]--;
+                    int num = i * 100 + j * 10 + k;
+                    ans.push_back(num);
+                    mp[k]++;
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
         return ans;
     }
 };
