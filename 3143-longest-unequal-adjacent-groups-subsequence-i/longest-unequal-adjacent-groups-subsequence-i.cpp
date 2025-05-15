7cdfee9a766cfe0115175c0caf85d7ff2c1770b6
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-05-15 21:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 29.9 MB (71.98%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,40 +1,13 @@
 class Solution {
 public:
-    int N;
-    vector<string> solve(int i, vector<string>& words, vector<int>& groups, vector<string>&temp, vector<int>&gTemp){
-        if(i >= N){
-            return temp;
-        }
-
-        // take 
-        if( groups[i] != gTemp.back()){
-            temp.push_back(words[i]);
-            gTemp.push_back(groups[i]);
-
-            return solve(i+1, words, groups, temp, gTemp);
-        }
-
-        // not take
-        return solve(i+1, words, groups, temp, gTemp);
-
-        
-    }
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
-        N=words.size();
-        int maxLen=-1;
-        vector<string>ans;
-        for(int i=0; i<N; i++){
-            vector<string>temp;
-            temp.push_back(words[i]);
-            vector<int>gTemp;
-            gTemp.push_back(groups[i]);
-            vector<string>v=solve(i+1, words, groups, temp, gTemp);
-            if((int)v.size() > maxLen){
-                maxLen=(int)v.size();
-                ans=v;
+        int N = words.size();
+        vector<string> ans;
+
+        for (int i = 0; i < N; i++) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                ans.push_back(words[i]);
             }
-            temp.erase(temp.begin());
-            gTemp.erase(gTemp.begin());
         }
         return ans;
     }
