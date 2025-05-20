786f4c5e7796fb950dc16f042550584c33feb84d
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-05-20 21:48:54 +0530

    Time: 618 ms (8.04%) | Memory: 337.3 MB (5.03%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,73 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* createParentMapping(TreeNode* root, int start, map<TreeNode*, TreeNode*>& m) {
-        TreeNode* res = NULL;
-
-        queue<TreeNode*> q;
-        q.push(root);
-        m[root]=NULL;
-
-        while (q.empty() == false) {
-            TreeNode* front = q.front();
-            q.pop();
-
-            if (front->val == start) {
-                res = front;
-            }
-            
-            if(front->left){
-                m[front->left]=front;
-                q.push(front->left);
-            }
-
-            if(front->right){
-                m[front->right]=front;
-                q.push(front->right);
-            }
+    void makeGraph(map<int, vector<int>>& adj, TreeNode* root, int parent) {
+        if (root == nullptr)
+            return;
+        if (parent != -1) {
+            adj[root->val].push_back(parent);
         }
-        return res;
-    }
-
-    void infectTree(TreeNode* root,map<TreeNode*, TreeNode*>& m,int &t){
-        map<TreeNode*,bool>visited;
-        queue<TreeNode*>q;
-
-        q.push(root);
-        visited[root]=true;
-
-        while(q.empty()==false){
-            int size=q.size();
-            bool flag=0;
-            for(int i=0;i<size;i++){
-                TreeNode* front=q.front();
-                q.pop();
-
-                if(front->left!=NULL && visited[front->left]==false){
-                    q.push(front->left);
-                    visited[front->left]=true;
-                    flag=1;
-                }
-                if(front->right!=NULL && visited[front->right]==false){
-                    q.push(front->right);
-                    visited[front->right]=true;
-                    flag=1;
-                }
-                if(m[front]!=NULL && visited[m[front]]==false){
-                    q.push(m[front]);
-                    visited[m[front]]=true;
-                    flag=1;
-                }
-            }
-            if(flag==1){
-                t++;
-            }
+        if (root->left) {
+            adj[root->val].push_back(root->left->val);
         }
+        if (root->right) {
+            adj[root->val].push_back(root->right->val);
+        }
+        makeGraph(adj, root->left, root->val);
+        makeGraph(adj, root->right, root->val);
     }
     int amountOfTime(TreeNode* root, int start) {
-        map<TreeNode*, TreeNode*> m;
-        int t = 0;
-        TreeNode* targetNode = createParentMapping(root, start, m);
-        infectTree(targetNode, m, t);
-        return t;
+        map<int, vector<int>> adj;
+        // make graph
+        makeGraph(adj, root, -1);
+
+        // apply bfs from start node
+        queue<int> q;
+        q.push(start);
+
+        unordered_set<int> visited;
+        visited.insert(start);
+
+        int minutes = 0;
+        while (!q.empty()) {
+            int N = q.size();
+
+            while (N--) {
+                int curr = q.front();
+                q.pop();
+
+                for (auto& i : adj[curr]) {
+                    if (visited.find(i) == visited.end()) {
+                        visited.insert(i);
+                        q.push(i);
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return minutes - 1;
     }
 };
