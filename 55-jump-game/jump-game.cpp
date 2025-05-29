cbe23fff96ed129050177137449469cd0202c36b
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-05-29 08:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (33.79%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int N = nums.size();
-        vector<int> t(N, false);
+        int maxReachable = 0;
 
-        // t[i] = true, means i tak pahucha ja sakta h
-        // t[i] = false, means i tak nhi pahucha ja sakta h
-
-        t[0] = true; // we can always reach 1st index
-
-        for (int i = 1; i < N; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (t[j] == true && j + nums[j] >= i) {
-                    t[i] = true;
-                    break;
-                }
+        for (int i = 0; i < N; i++) {
+            if(i > maxReachable){
+                return false;
             }
+            maxReachable = max(maxReachable, i + nums[i]);
         }
-        return t[N - 1];
+        return true;
     }
 };
