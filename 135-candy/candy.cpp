a86f25583ab1fefe8713d7272a35d4e842d499f7
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-06-02 21:14:27 +0530

    Time: 8 ms (9.92%) | Memory: 23.9 MB (6.39%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int candy(vector<int>& rating) {
         int N=rating.size();
-        vector<int>v(N, 1);
+        vector<int>L2R(N, 1);
 
         // L->R
         for(int i=1; i<N; i++){
             if(rating[i] > rating[i-1]){
-                v[i]=v[i-1]+1;
+                L2R[i]=L2R[i-1]+1;
             }
         }
 
         // R->L
-        vector<int>v2(N,1);
+        vector<int>R2L(N,1);
         for(int i=N-2; i>=0; i--){
             if(rating[i] > rating[i+1]){
-                v2[i]=v2[i+1]+1;
+                R2L[i]=R2L[i+1]+1;
             }
         }
+        
         vector<int>ans(N, 0);
         for(int i=0; i<N; i++){
-            ans[i]=max(v[i], v2[i]);
+            ans[i]=max(L2R[i], R2L[i]);
         }
         int sum=accumulate(begin(ans), end(ans), 0);
         return sum;
