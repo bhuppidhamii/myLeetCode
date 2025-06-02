282a02c09dfdd9e44e2dbe228241b2e39babc33b
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-06-02 08:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (33.33%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,23 +1,24 @@
 class Solution {
-private:
-    int solve(int i, int j, int m, int n, vector<vector<int>> &dp){
-        if(i >=m || j>=n) return 0;
-        
-        if(i == m-1 && j == n-1){
-            return 1; // we have found 1 path.
-        }
-
-        if(dp[i][j] != -1){ // we have al ready visited this path.
-            return dp[i][j]; 
-        }
-
-        dp[i][j] = solve(i+1, j, m, n, dp) + solve(i, j+1, m, n, dp);
-        // memoization
-        return dp[i][j];
-    }
 public:
+    int solve(int i, int j, int& m, int& n, vector<vector<int>>& dp) {
+        if (i > m-1 || j > n-1) // robot out of matrix
+            return 0;
+
+        if (i == m - 1 && j == n - 1) // robot has reached the target
+            return 1;
+        
+        // cout<<i<<" - "<<j<<"\n";
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int left = solve(i + 1, j, m, n, dp);
+
+        int down = solve(i, j + 1, m, n, dp);
+
+        return dp[i][j] = left + down; // Memoization
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
         return solve(0, 0, m, n, dp);
     }
 };
