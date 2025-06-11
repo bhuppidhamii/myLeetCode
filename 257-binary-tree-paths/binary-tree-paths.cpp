ad45cf67b4e57b3c9ea6e719c6e019f624af542e
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-06-11 21:24:15 +0530

    Time: 3 ms (30.71%) | Memory: 17.9 MB (26.37%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,22 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> ans;
-        string path = "";
-        dfs(root, path, ans);
-        return ans;
-    }
-    void dfs(TreeNode* root, string path, vector<string>& ans) {
-        if (root == nullptr)
+    void solve(TreeNode *root, string path, vector<string>&ans){
+        if(root==NULL){
             return;
-
-        path += to_string(root->val);
-
-        if (root->left == NULL && root->right == NULL) {
-            ans.push_back(path);
-        } else {
-            dfs(root->left, path + "->", ans);
-            dfs(root->right, path + "->", ans);
         }
+        path+=to_string(root->val);
+        path+="->";
+        if(root->left==NULL && root->right==NULL){
+            path.pop_back();
+            path.pop_back();
+            ans.push_back(path);
+            return;
+        }
+
+        solve(root->left, path, ans);
+
+        solve(root->right, path, ans);
+        path.pop_back();
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>ans;
+        solve(root,"", ans);
+        // for(auto &i:ans){
+        //     cout<<i<<" ";
+        // }
+        return ans;
     }
 };
