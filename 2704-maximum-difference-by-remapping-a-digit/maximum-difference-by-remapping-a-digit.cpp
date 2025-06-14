48fee00a0d3b91eafbb8bc5dda3b56c24823b47d
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-06-14 10:56:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (37.74%) - LeetSync

--- 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -1,50 +1,33 @@
 class Solution {
 public:
-    void changeMax(string &str, char toChange){
-        for(int i=0; i<str.size(); i++){
-            if(str[i] == toChange){
-                str[i]='9';
-            }
-        }
-    }
-    void changeMin(string &str, char toChange2){
-        for(int i=0; i<str.size(); i++){
-            if(str[i] == toChange2){
-                str[i]='0';
-            }
-        }
-    } 
     int minMaxDifference(int num) {
-        // Jai Shri Ram
-        // find max
-        // change the 1st non "9" occurence & its counterparts to "9".
-        string str1=to_string(num);
-        int N=str1.size();
-        char toChange;
-        for(int i=0; i<N; i++){
-            if(str1[i] != '9'){
-                toChange=str1[i];
-                break;
-            }
-        }
-        changeMax(str1, toChange);
-        int maxi=stoi(str1);
-        cout<<maxi<<"\n";
+        // find maximum number --------
 
-        // find min
-        // change the 1st non '0' occurence & its counterparts to '0'
-        string str2=to_string(num);
-        char toChange2;
-        for(int i=0; i<N; i++){
-            if(str1[i] != '0'){
-                toChange2=str2[i];
-                break;
-            }
+        string str1 = to_string(num);
+        // find the 1st index of char, which is not '9'
+        int idx1 = str1.find_first_not_of('9');
+
+        if (idx1 != string::npos) {
+            char ch = str1[idx1];
+
+            // replace every occurance of ch with '9'
+            replace(str1.begin(), str1.end(), ch, '9');
         }
-        changeMin(str2, toChange2);
-        // cout<<str2;
-        int mini=stoi(str2);
-        cout<<mini;
-        return maxi-mini;
+        int maxi = stoi(str1);
+
+        // find minimum number --------
+        string str2 = to_string(num);
+        // find the 1st index of char, which is not '0
+        int idx2 = str2.find_first_not_of('0');
+
+        if (idx2 != string::npos) {
+            char ch = str2[idx2];
+
+            // replace every occurence of ch wiht '0'
+            replace(str2.begin(), str2.end(), ch, '0');
+        }
+        int mini = stoi(str2);
+
+        return maxi - mini;
     }
 };
