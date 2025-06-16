48a16813701235b8f633ec1e5e684b04f98d81a3
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-06-16 21:36:55 +0530

    Time: 44 ms (5.14%) | Memory: 20 MB (5.12%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,34 +1,46 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
+    // vector<vector<int>>paths;
+    bool findPath(TreeNode *root, int target, vector<int>&path){
+        if(root==NULL) return false;
+
+        path.push_back(root->val);
+        if(root->val==target) return true;
+
+        if(findPath(root->left, target, path) || findPath(root->right, target, path)){
+            return true;
+        }
+
+        path.pop_back();
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root) {
-            return NULL;
+        // Jai Shri Ram
+        vector<int>path1;
+        findPath(root, p->val, path1);
+
+        vector<int>path2;
+        findPath(root, q->val, path2);
+
+        map<int,int>mp;
+        for(auto &i:path1){
+            cout<<i<<" ";
+            mp[i]++;
+        }
+        cout<<"\n";
+        for(auto &i:path2){
+            cout<<i<<" ";
+            mp[i]++;
+        }
+        cout<<"\n";
+        int i = 0;
+        while(i < path1.size() && i < path2.size() && path1[i] == path2[i]) {
+            i++;
         }
 
-        if (root == p || root == q) {
-            return root;
-        }
-
-        TreeNode* leftN = lowestCommonAncestor(root->left, p, q);
-        TreeNode* rightN = lowestCommonAncestor(root->right, p, q);
-
-        if (leftN != NULL && rightN != NULL) {
-            return root;
-        }
-
-        if (leftN != NULL) {
-            return leftN;
-        }
-
-        return rightN;
+        int lca_val = path1[i-1]; // common ancestor value
+        // if both the datas are same, return last node
+        TreeNode * newNode = new TreeNode(lca_val);
+        return newNode;
     }
 };
