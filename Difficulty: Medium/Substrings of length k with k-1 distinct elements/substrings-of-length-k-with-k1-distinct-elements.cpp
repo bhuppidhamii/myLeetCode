5a75dd45a06a988baaee62d39b0ec9e63bed978a
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-07-01 08:08:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings of length k with k-1 distinct elements/substrings-of-length-k-with-k1-distinct-elements.cpp
+++ Difficulty: Medium/Substrings of length k with k-1 distinct elements/substrings-of-length-k-with-k1-distinct-elements.cpp
@@ -2,31 +2,34 @@ class Solution {
   public:
     int substrCount(string &s, int k) {
         // Jai Shri Ram
-        if(k > s.size()) return 0;
+        vector<int>v(26, 0);
         
-        map<char, int>mp;
+        // increase count+1 if you increase any count from 0
+        // decrease count-1 if you reduce any count to 0
+        int c = 0;
         for(int i=0; i<k; i++){
-            mp[s[i]]++;
+            int idx = s[i]-'a';
+            if(v[idx] == 0) c++;
+            v[idx]++; // increase frequency
         }
-        
-        int count=0;
-        if(mp.size() == k-1){
-            count++;
-        }
+        int ans = 0;
+        // cout<<c;
+        if(c == k-1) ans++;
         
         int i=0;
         for(int j=k; j<s.size(); j++){
-            mp[s[i]]--;
-            if(mp[s[i]] == 0){
-                mp.erase(s[i]);
+            int idx = s[i]-'a';
+            v[idx]--; // decrease frequency
+            if(v[idx] == 0){
+                c--;
             }
             i++;
             
-            mp[s[j]]++;
-            if(mp.size() == k-1){
-                count++;
-            }
+            if(v[s[j]-'a'] == 0) c++;
+            v[s[j]-'a']++; // increase frequency
+            
+            if(c == k-1) ans++; // check
         }
-        return count;
+        return ans;
     }
 };
