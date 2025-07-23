a5c9e8835cbe5baf3c6b2bf466bd60875d1943ac
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-07-23 10:01:16 +0530

    Time: 61 ms (36.62%) | Memory: 27.4 MB (64.67%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,67 +1,53 @@
 class Solution {
 public:
+    int res;
+    string removeAB(string& s, int x) {
+        string str = "";
+        for (auto& ch : s) {
+            if (str.empty()) {
+                str += ch;
+            } else {
+                if (str.back() == 'a' && ch == 'b') {
+                    str.pop_back();
+                    res += x;
+                } else {
+                    str += ch;
+                }
+            }
+        }
+        return str;
+    }
+    string removeBA(string& s, int y) {
+        string str = "";
+        for (auto& ch : s) {
+            if (str.empty()) {
+                str += ch;
+            } else {
+                if (str.back() == 'b' && ch == 'a') {
+                    str.pop_back();
+                    res += y;
+                } else {
+                    str += ch;
+                }
+            }
+        }
+        return str;
+    }
     int maximumGain(string s, int x, int y) {
-        int res = 0;
+        res = 0;
         if (x >= y) {
             // remove ab first
-            string str = "";
-            for (auto& ch : s) {
-                if (str.empty()) {
-                    str += ch;
-                } else {
-                    if (str.back() == 'a' && ch == 'b') {
-                        str.pop_back();
-                        res += x;
-                    } else {
-                        str += ch;
-                    }
-                }
-            }
+            string str = removeAB(s, x);
 
             // remove ba
-            string str2 = "";
-            for (auto& ch : str) {
-                if (str2.empty()) {
-                    str2 += ch;
-                } else {
-                    if (str2.back() == 'b' && ch == 'a') {
-                        str2.pop_back();
-                        res += y;
-                    } else {
-                        str2 += ch;
-                    }
-                }
-            }
+            string str2 = removeBA(str, y);
+
         } else {
-            // remove ba
-            string str = "";
-            for (auto& ch : s) {
-                if (str.empty()) {
-                    str += ch;
-                } else {
-                    if (str.back() == 'b' && ch == 'a') {
-                        str.pop_back();
-                        res += y;
-                    } else {
-                        str += ch;
-                    }
-                }
-            }
+            // remove ba first
+            string str = removeBA(s, y);
 
             // remove ab
-            string str2 = "";
-            for (auto& ch : str) {
-                if (str2.empty()) {
-                    str2 += ch;
-                } else {
-                    if (str2.back() == 'a' && ch == 'b') {
-                        str2.pop_back();
-                        res += x;
-                    } else {
-                        str2 += ch;
-                    }
-                }
-            }
+            string str2 = removeAB(str, x);
         }
         return res;
     }
