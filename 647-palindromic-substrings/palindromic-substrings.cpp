872e765b6891b68b1ba22263021bc921d335b6a5
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-08-10 10:16:06 +0530

    Time: 540 ms (14.41%) | Memory: 473 MB (5.16%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    int t[1001][1001];
-    bool solve(int i, int j, string& s) {
+    bool check(string& s, int i, int j, vector<vector<int>>& dp) {
         if (i >= j)
             return true;
 
-        if (t[i][j] != -1) {
-            return t[i][j];
-        }
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
         if (s[i] == s[j]) {
-            return t[i][j] = solve(i + 1, j - 1, s);
+            return dp[i][j] = check(s, i + 1, j - 1, dp);
+        } else {
+            return dp[i][j] = false;
         }
-        return t[i][j] = false;
     }
     int countSubstrings(string s) {
-        int n = s.size();
-        int i = 0, j = n - 1;
-        memset(t, -1, sizeof(t));
-
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (solve(i, j, s) == true) { // if substr is palindrome
-                    cnt++;
+        int N = s.size(), count = 0;
+        vector<vector<int>> dp(N + 1, vector<int>(N + 1, -1));
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                if (check(s, i, j, dp)) {
+                    string str = s.substr(i, j - i + 1);
+                    if (str.size() > 0) {
+                        count++;
+                        // cout<<str<<"\n";
+                    }
                 }
             }
         }
-        return cnt;
+        return count;
     }
 };
