c8cc0014e45a350ba2771285c51c685527d72104
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-08-11 21:21:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
@@ -9,14 +9,13 @@ class Solution {
             return solve(i+1, j-1, s, dp);
         }
         
-        int move_i =  1 + solve(i+1, j, s, dp);
-        int move_j =  1 + solve(i, j-1, s, dp);
+        int move_i = 1 + solve(i+1, j, s, dp);
+        int move_j = 1 + solve(i, j-1, s, dp);
         
-        return dp[i][j] = min({move_i, move_j});
+        return dp[i][j] = min(move_i, move_j);
     }
-    int findMinInsertions(string &s) {
+    int countMin(string s) {
         // Jai Shri Ram
-        
         int N = s.size();
         vector<vector<int>>dp(N+1, vector<int>(N+1, -1));
         return solve(0, N-1, s, dp);
