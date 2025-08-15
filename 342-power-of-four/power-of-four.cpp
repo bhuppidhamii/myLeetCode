1b28035e45b467e9188b9d21598d59480760dd26
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-08-15 09:49:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.41%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n < 1)
-            return false;
+        if(n < 1) return false;
+        double x = log10(n) / log10(4);
 
-        while (n % 4 == 0) {
-            n /= 4;
-        }
-        return n == 1;
+        return x == (int)x;
     }
 };
