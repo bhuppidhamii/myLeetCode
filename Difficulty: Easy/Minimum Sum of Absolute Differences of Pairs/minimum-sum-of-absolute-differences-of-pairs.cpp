9b25081f6b18848af1103d19f5abfe8a13ee7fdf
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-08-17 07:39:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Sum of Absolute Differences of Pairs/minimum-sum-of-absolute-differences-of-pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    long long findMinSum(vector<int> &A, vector<int> &B, int N) {
+        // Jai Shri Ram
+        sort(A.begin(), A.end());
+        sort(B.begin(), B.end());
+        
+        long long ans = 0;
+        for(int i=0; i<N; i++){
+            ans+=abs(A[i] - B[i]);
+        }
+        return ans;
+    }
+};
