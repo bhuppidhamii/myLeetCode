11298148e5125e9c1a46daa9746ea1c9d570f466
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-08-23 10:45:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,33 +1,32 @@
 class Solution {
   public:
-    bool valid(vector<int>&arr, int k, int mid){
-        int parts=0;
-        int i=0, N=arr.size();
-        int curr=0;
+    bool check(vector<int> &arr, int k, int mid){
+        int parts = 0;
+        int sum = 0;
+        int N = arr.size(), i = 0;
+        
         while(i < N){
-            int j=i;
-            while(j < N && curr+arr[j] <= mid){
-                curr+=arr[j];
-                j++;
+            
+            while(i<N && sum+arr[i] <= mid){
+                sum+=arr[i];
+                i++;
             }
             parts++;
-            curr=0;
-            i=j;
+            sum = 0;
         }
         return parts <= k;
     }
     int findPages(vector<int> &arr, int k) {
         // Jai Shri Ram
-        int N = arr.size();
-        if(k > N) return -1;
+        if(k > arr.size()) return -1;
+        int low = *max_element(arr.begin(), arr.end());
+        int high = accumulate(arr.begin(), arr.end(), 0);
         
-        int low=*max_element(begin(arr), end(arr));
-        int high=accumulate(begin(arr), end(arr), 0);
-        
-        int ans=-1;
+        int ans = -1;
         while(low <= high){
-            int mid=low+(high-low)/2;
-            if(valid(arr, k, mid)){
+            int mid = low +(high-low)/2;
+            
+            if(check(arr, k, mid) == true){
                 ans = mid;
                 high = mid-1;
             }else{
@@ -35,5 +34,6 @@ class Solution {
             }
         }
         return ans;
+        // return check(arr, k, 113);
     }
 };
