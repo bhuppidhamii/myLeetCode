2b24db23bb37867615b620619f6ce214cc5a18fb
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-08-30 18:18:17 +0530

    Time: 7 ms (35.47%) | Memory: 26 MB (7.64%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,45 +2,23 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
 
-        // validate rows
-        for (int row = 0; row < 9; row++) {
-            unordered_set<char> st;
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') continue;
-                if (st.find(board[row][col]) != st.end()) {
-                    return false;
-                }
-                st.insert(board[row][col]);
-            }
-        }
+        set<string>st;
+        for(int i= 0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j]=='.') continue;
 
-        // validate columns
-        for (int col = 0; col < 9; col++) {
-            unordered_set<char> st;
-            for (int row = 0; row < 9; row++) {
-                if (board[row][col] == '.') continue;
-                if (st.find(board[row][col]) != st.end()) {
-                    return false;
-                }
-                st.insert(board[row][col]);
-            }
-        }
+                string row = string(1, board[i][j]) + "ROW" + to_string(i);
+                string col = string(1, board[i][j]) + "COL" + to_string(j);
+                string box = string(1, board[i][j]) + "BOX" + to_string(i/3) + "_" + to_string(j/3);
 
-        // validate 3x3 sub-boxes
-        for(int row = 0; row<9; row+=3){
-            for(int col = 0; col<9; col+=3){
+                if(st.find(row) != st.end()) return false;
+                st.insert(row);
 
-                // each 3x3 box
-                unordered_set<char> st;
-                for(int i=row; i<row+3; i++){
-                    for(int j=col; j<col+3; j++){
-                        if (board[i][j] == '.') continue;
-                        if (st.find(board[i][j]) != st.end()) {
-                            return false;
-                        }
-                        st.insert(board[i][j]);
-                    }
-                }
+                if(st.find(col) != st.end()) return false;
+                st.insert(col);
+
+                if(st.find(box) != st.end()) return false;
+                st.insert(box);
             }
         }
         return true;
