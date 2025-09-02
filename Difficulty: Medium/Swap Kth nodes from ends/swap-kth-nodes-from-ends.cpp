9618fbfd073ae4778af4e76a830acd2f3949dd5a
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-09-02 08:16:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.cpp
+++ Difficulty: Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.cpp
@@ -16,29 +16,39 @@ class Solution {
     Node* swapKth(Node* head, int k) {
         // Jai Shri Ram
         
-        // BF
-        vector<int>v;
+        // little optimal
+        int N = 0;
         Node *temp = head;
         while(temp){
-            v.push_back(temp->data);
+            N++;
             temp=temp->next;
         }
         
-        int N = v.size();
-        if(k > N) return head; // if not possible
+        if(k>N) return head;
         
-        swap(v[k-1], v[N-k]);  // swap
-        
-        Node * dummy = new Node(0); // create a new node
-        temp = dummy;
-        
-        for(auto &i:v){
-            Node *newNode = new Node(i);
-            temp->next=newNode;
+        // find the pos of kth node
+        int i = 1;
+        temp=head;
+        while(i<k){
             temp=temp->next;
+            i++;
         }
+        Node * a = temp;
         
-        return dummy->next;
+        // find pos of N-kth node
+        i=0;
+        temp=head;
+        while(i < N-k){
+            temp=temp->next;
+            i++;
+        }
+        Node *b = temp;
         
+        // swap
+        int c=a->data;
+        a->data=b->data;
+        b->data=c;
+        
+        return head;
     }
 };
