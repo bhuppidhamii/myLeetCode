f81f2f4220f9a9fd82ebff697b59eee44891661d
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-09-11 09:07:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,27 +1,24 @@
 class Solution {
-  public:
-    int N;
-    int solve(vector<int>&arr, int i, vector<int>&dp){
-        if(i >= N-1) return 0;
-        if(arr[i] == 0) return INT_MAX;
-        if(dp[i] != -1) return dp[i];
-        
-        int maxJumps=arr[i];
-        int minJumps=INT_MAX;
-        for(int j=1; j<=maxJumps&&i+j<N; j++){
-            int temp=solve(arr, i+j, dp);
-            if(temp != INT_MAX){
-                minJumps=min(minJumps, temp+1);
+public:
+    int minJumps(vector<int>& arr) {
+        // Jai Shri Ram
+        int n = arr.size();
+        if (n == 1) return 0;       // already at last index
+        if (arr[0] == 0) return -1; // cannot move anywhere
+
+        int jumps = 0, farthest = 0, end = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, i + arr[i]);
+
+            // When we reach the end of the current jump range
+            if (i == end) {
+                jumps++;
+                end = farthest;
+
+                if (end >= n - 1) return jumps; // reached last index
             }
         }
-        return dp[i]=minJumps;
-    }
-    int minJumps(vector<int>& arr) {
-        // code here
-        N=arr.size();
-        vector<int>dp(N+1, -1);
-        int ans= solve(arr, 0, dp);
-        if(ans==INT_MAX) return -1;
-        return ans;
+        return -1; // can't reach last index
     }
 };
