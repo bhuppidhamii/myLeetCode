59ee55f86aeeef77f63676632d4f9f7c7655fb14
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-09-12 10:21:01 +0530

    Time: 28 ms (5.04%) | Memory: 19.3 MB (14.55%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        auto lambda = [](char ch) {
-            return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
-        };
-        return any_of(begin(s), end(s), lambda);
+        return any_of(begin(s), end(s), [](char ch) {
+            return string("aeiou").find(ch) != string::npos;
+        });
     }
 };
