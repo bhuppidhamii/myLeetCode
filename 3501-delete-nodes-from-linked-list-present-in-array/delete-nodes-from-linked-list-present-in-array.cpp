c0217bb14fe9ae6df3541af051282e3153bcfa23
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-09-16 09:25:42 +0530

    Time: 112 ms (32.69%) | Memory: 260.5 MB (90.49%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        set<int>st(begin(nums), end(nums));
-
-        // BF
-        ListNode *temp = head;
-        vector<int>v;
-        while(temp){
-            if(st.find(temp->val) == st.end()){ // not found in set
-                v.push_back(temp->val);
-            }
-            temp=temp->next;
-        }
+        // Jai Shri Ram
+        unordered_set<int>st(begin(nums), end(nums));
         
         ListNode *dummy = new ListNode(0);
-        temp = dummy;
-        for(auto &i:v){
-            ListNode *newNode = new ListNode(i);
-            temp->next = newNode;
+        ListNode *d = dummy;
+
+        ListNode *prev = NULL;
+        ListNode *temp = head;
+
+        while(temp){
+            if(st.find(temp->val) == st.end()){ // not present in set
+                d->next = temp;
+                d = d->next;
+            }
+            prev = temp;
             temp = temp->next;
+            prev->next = NULL;
         }
         return dummy->next;
     }
