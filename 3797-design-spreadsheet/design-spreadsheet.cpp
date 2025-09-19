7dc4d8cab05c5051b3201ddc0ff6be2a3edf0386
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-09-19 12:05:48 +0530

    Time: 140 ms (25.00%) | Memory: 192.5 MB (25.53%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.cpp
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -1,79 +1,44 @@
 class Spreadsheet {
 public:
-    pair<int,int> parseCell(string cell){
-        int col = cell[0] - 'A';       // column index
-        int row = stoi(cell.substr(1)) - 1; // row index (convert 1-based to 0-based)
-        return make_pair(row, col);
+    vector<vector<int>> sp;
+
+    Spreadsheet(int rows) {
+        sp.resize(rows, vector<int>(26, 0)); // rows × 26 grid
     }
 
-    vector<vector<int>>sp;
-    Spreadsheet(int rows) {
-        sp.resize(rows, vector<int>(26));
+    pair<int,int> parseCell(const string& cell) {
+        int col = cell[0] - 'A';                 // column (0-based)
+        int row = stoi(cell.substr(1)) - 1;      // row (1-based → 0-based)
+        return {row, col};
     }
-    
+
     void setCell(string cell, int value) {
-        pair<int,int>p = parseCell(cell);
-        int r = p.first;
-        int c = p.second;
-        
+        auto [r, c] = parseCell(cell);
         sp[r][c] = value;
     }
-    
-    void resetCell(string cell) {
-        pair<int,int>p = parseCell(cell);
-        int r = p.first;
-        int c = p.second;
 
+    void resetCell(string cell) {
+        auto [r, c] = parseCell(cell);
         sp[r][c] = 0;
     }
-    
+
     int getValue(string formula) {
-        int idx = formula.find_first_of('+');
-        string a = formula.substr(1, idx-1);
-        string b = formula.substr(idx+1);
+        // formula looks like "=X+Y"
+        formula = formula.substr(1); // remove '='
+        int plusPos = formula.find('+');
+        string a = formula.substr(0, plusPos);
+        string b = formula.substr(plusPos + 1);
 
-        
-        // check if a is all digits like 7 or alnum like A1
-        string a1 = a;
-        sort(begin(a1), end(a1));
-        cout<<"a1 "<<a1<<"\n";
-        int n1 = 0;
-        // if after sorting a -> it is like 1A -> then it is alnum
-        if(isdigit(a1[a1.size()-1]) == true){ // then its a number
-            n1 = stoi(a);
-        }else{
-            // its a A1 like format
-            pair<int,int>p = parseCell(a);
-            int r = p.first;
-            int c = p.second;
-            cout<<"here";
-            n1 = sp[r][c];
+        return getOperandValue(a) + getOperandValue(b);
+    }
+
+private:
+    int getOperandValue(const string& token) {
+        if (isdigit(token[0])) {
+            return stoi(token); // it's a number
+        } else {
+            auto [r, c] = parseCell(token);
+            return sp[r][c];   // it's a cell
         }
-        cout<<"n1 "<<n1<<" ";
-
-        // for b also
-        string b1 = b;
-        sort(begin(b1), end(b1));
-        // cout<<" b1 "<<b1<<"\n";
-        int n2 = 0;
-        if(isdigit(b1[b1.size()-1])==true){
-            n2 = stoi(b);
-        }else{
-            pair<int,int>p = parseCell(b);
-            int r = p.first;
-            int c = p.second;
-
-            n2= sp[r][c];
-        }
-        // cout<<"n2 "<<n2<<"\n";
-        return n1+n2;
     }
 };
-
-/**
- * Your Spreadsheet object will be instantiated and called as such:
- * Spreadsheet* obj = new Spreadsheet(rows);
- * obj->setCell(cell,value);
- * obj->resetCell(cell);
- * int param_3 = obj->getValue(formula);
- */
