e0fe94fd33a7c59e54f8beae347648e81fcab7d1
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-09-23 09:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (30.92%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-       int L1 = version1.size(), i = 0;
-       int L2 = version2.size(), j = 0;
+        stringstream ss1(version1);
 
-       while(i < L1 || j < L2){
-        
-        // gather all elements till '.'
-        string s1 = "";
-        while(i < L1 && version1[i] != '.'){
-            s1+=version1[i];
-            i++;
+        stringstream ss2(version2);
+
+        string word1, word2;
+        while (ss1.good() || ss2.good()) {
+            if (!getline(ss1, word1, '.')) word1 = "0";
+            if (!getline(ss2, word2, '.')) word2 = "0";
+            
+            int n1 = stoi(word1);
+            int n2 = stoi(word2);
+
+            if (n1 < n2) return -1;
+            if (n1 > n2) return 1;
         }
-        cout<<i<<" ";
-        int n1 = (s1.size()==0) ? 0 : stoi(s1);
-        cout<<n1<<" \n";
-        
-        string s2 = "";
-        while(j < L2 && version2[j] != '.'){
-            s2+=version2[j];
-            j++;
-        }
-        cout<<j<<" ";
-
-        int n2 = (s2.size()==0) ? 0 : stoi(s2);
-        cout<<n2<<" \n";
-
-        // compare them
-        if(n1 < n2) return -1;
-        if(n1 > n2) return 1;
-
-        // cout<<i<<" "<<j<<"\n";
-        i++,j++;
-       }
-       return 0;
+        return 0;
     }
 };
