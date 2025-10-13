a45ba9bd26e33d09f2901f43bfb19f2596da98d7
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-10-13 09:30:08 +0530

    Time: 23 ms (5.06%) | Memory: 25.2 MB (5.78%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
+    bool check(string &word1, string &word2){
+        map<char, int>mp1;
+        for(auto &ch:word1){
+            mp1[ch]++;
+        }
+
+        map<char, int>mp2;
+        for(auto &ch:word2){
+            mp2[ch]++;
+        }
+
+        return mp1==mp2;
+    }
     vector<string> removeAnagrams(vector<string>& words) {
-        int n=words.size();
+        // Jai Shri Ram
+        int N = words.size();
+        int i=0,j=0;
         vector<string>ans;
-        
-        string str=words[0];
-        ans.push_back(str);
-        sort(str.begin(), str.end());
-
-        for(int i=1;i<n;i++){
-            string str1=words[i];
-            sort(str1.begin(), str1.end());
-
-            if(str1 != str){
-                ans.push_back(words[i]);
-                str=str1;
+        while(i < N){
+            int j=i+1;
+            while(j<N && check(words[i], words[j])==true){
+                j++;
             }
+            ans.push_back(words[i]);
+            i=j;
         }
         return ans;
     }
