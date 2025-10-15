b8c5e39672ce6d450d95e69295caf38ecf5e0ce4
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-10-15 08:30:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.cpp
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.cpp
@@ -1,19 +1,21 @@
+
 class Solution {
   public:
-    void solve(Node *root, vector<int>&v){
-        if(root!=NULL){
-            solve(root->left, v);
-            v.push_back(root->data);
-            solve(root->right, v);
+    int res=-1;
+    void solve(Node *root, int &k){
+        if(root == NULL) return;
+        
+        solve(root->left, k);
+        k--;
+        if(k == 0) {
+            res = root->data; 
+            return;
         }
+        solve(root->right, k);
     }
     int kthSmallest(Node *root, int k) {
         // Jai Shri Ram
-        // BF
-        vector<int>v;
-        solve(root, v);
-        
-        if(k > v.size()) return -1;
-        return v[k-1];
+        solve(root, k);
+        return res;
     }
 };
