1128722d9bb8fb01843fea04ef34c7bc6bc5a919
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-10-27 20:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (54.97%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
+    int countLaser(string &b){
+        int count = 0;
+        for(auto &ch:b){
+            if(ch=='1') count++;
+        }
+        return count;
+    }
     int numberOfBeams(vector<string>& bank) {
-        int N = bank.size();
-        vector<int> v;
-        for (int i = 0; i < N; i++) {
-            int count1s = 0;
-            for (auto ch : bank[i]) {
-                if (ch == '1') {
-                    count1s++;
-                }
-            }
-            if (count1s != 0) {
-                v.push_back(count1s);
+        vector<int>v;
+        for(auto &b:bank){
+            int count = countLaser(b);
+            if(count != 0){
+                v.push_back(count);
             }
         }
-        if(v.size()==0 ||v.size()==1){
-            return 0;
-        }
+
         int ans = 0;
-        for (int i = 0; i < v.size() - 1; i++) {
-            int prod = v[i] * v[i + 1];
-            ans += prod;
+        for(int i=1; i<v.size(); i++){
+            ans += (v[i]*v[i-1]);
         }
         return ans;
     }
