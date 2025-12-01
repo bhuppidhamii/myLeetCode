23a0dae31cd4d74d9327a92fad94927e6a8c58bc
Bhupender Singh <dhamibhupendra3@gmail.com>
2025-12-01 10:05:03 +0530

    Time: 63 ms (8.38%) | Memory: 36.8 MB (7.03%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int dupNum=0;
-        map<int,int>m;
-        for(auto i:nums){
-            m[i]++;
-        }
-        for(auto i:m){
-            if(i.second == 2){
-                dupNum=i.first;
-                break;
+        sort(begin(nums), end(nums));
+        map<int, int> mp;
+        int twice = 0, missing = 0, n = 1;
+        for (auto& i : nums) {
+            mp[i]++;
+
+            // twice
+            if (mp[i] > 1) {
+                twice = i;
             }
-        }
 
-        int nSum=0;
-        for(int i=1;i<=nums.size();i++){
-            nSum+=i;
+            // missing
+            if (mp.find(n) == mp.end()) {
+                missing = n;
+            }
+            n++;
         }
-
-        int numsSum=0;
-        for(auto i:m){
-            numsSum+=i.first;
-        }
-
-        return {dupNum, nSum-numsSum};
+        return {twice, missing};
     }
 };
