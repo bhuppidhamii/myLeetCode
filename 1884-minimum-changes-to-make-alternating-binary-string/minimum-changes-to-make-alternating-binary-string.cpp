ca841199b560bb4226290cd77948cec712396332
Bhupender Singh <dhamibhupendra3@gmail.com>
2026-03-05 09:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (95.63%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -6,24 +6,14 @@ public:
 
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
-                // every even idx -> 0
                 if (s[i] != '0') count1++;
-            } else {
-                // every odd idx -> 1
-                if (s[i] != '1') count1++;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                // every even idx -> 1
                 if (s[i] != '1') count2++;
             } else {
-                // every odd idx -> 0
+                if (s[i] != '1') count1++;
                 if (s[i] != '0') count2++;
             }
         }
-
+        
         return min(count1, count2);
     }
 };
