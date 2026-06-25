d940a28c0688236ce8147fc5e2f951a1ce1cac70
Bhupender Singh <dhamibhupendra3@gmail.com>
2026-06-25 09:19:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/N-Digit Numbers with Increasing Digits/ndigit-numbers-with-increasing-digits.cpp
+++ Difficulty: Medium/N-Digit Numbers with Increasing Digits/ndigit-numbers-with-increasing-digits.cpp
@@ -2,19 +2,12 @@ class Solution {
   public:
     void solve(int n, vector<int>&ans, string &temp){
         if(temp.size() == n){
-            // cout<<temp<<"\n";
             ans.push_back(stoi(temp));
             return;
         }
         
         for(int i=1; i<=9; i++){
-            if(temp.size() != 0 && i > temp.back()-'0'){
-                temp+=(i+'0');
-                solve(n, ans, temp);
-                temp.pop_back();
-            }
-            
-            if(temp.size() == 0){
+            if(temp.size() == 0 || i > temp.back()-'0'){
                 temp+=(i+'0');
                 solve(n, ans, temp);
                 temp.pop_back();
